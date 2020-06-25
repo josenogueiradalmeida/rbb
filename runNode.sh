923e83f04595a43fabe4c36e7f12dfaa54ba8d39
José Nogueira <josej@bdnes.gov.br>
2020-06-25 16:51:02 -0300

    .

--- runNode.sh
+++ runNode.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 #RUN ONE TYPE OF NODE
 
+TAG=v0.01
+
 if [[ $# -eq 0 ]] ; then
     echo 'informe o tipo de no desejado: boot, validator ou regular'
     exit 0
 fi
 
-sudo docker pull nogueiradalmeida/rbbnode:v0.01
+sudo docker pull nogueiradalmeida/rbbnode:$TAG
 echo NODE $1
 cd node$1
 if [ "$2" = "clear" ]; then
@@ -20,4 +22,4 @@ if [ "$2" = "clear" ]; then
     sudo rm -rf uploads
     cd ..
 fi
-sudo docker run --network=host -v $PWD/:/conf/local -v $PWD/data:/conf/data rbb_bootnode:latest
+sudo docker run --network=host -v $PWD/:/conf/local -v $PWD/data:/conf/data rbb_bootnode:$TAG
